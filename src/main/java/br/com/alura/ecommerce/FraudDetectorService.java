c1f457388d23b28af82ff75c3551cd1d71a1f261
Viviane Scarlatti <viviane.scarlatti@gmail.com>
2021-05-24 22:02:36 -0300

    Incluindo deserialização customizada

--- src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -2,16 +2,23 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FraudDetectorService {
 
     public static void main(String[] args) {
         FraudDetectorService fraudService = new FraudDetectorService();
-        try (KafkaService service = new KafkaService(EmailService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER", fraudService::parse)) {
+        try (KafkaService service = new KafkaService<>(EmailService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudService::parse,
+                Order.class,
+                Map.of())) {
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String, String> record) {
+    private void parse(ConsumerRecord<String, Order> record) {
         System.out.println("----------------------------------------");
         System.out.println("Processing new order, checking for fraud");
         System.out.println(record.key());
--- src/main/java/br/com/alura/ecommerce/LogService.java
+++ src/main/java/br/com/alura/ecommerce/LogService.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
@@ -17,7 +18,9 @@ public class LogService {
 
         try (var service = new KafkaService(LogService.class.getSimpleName(),
                 Pattern.compile("ECOMMERCE.*"),
-                logService::parse)) {
+                logService::parse,
+                String.class,
+                Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()))) {
             service.run();
         }
     }
